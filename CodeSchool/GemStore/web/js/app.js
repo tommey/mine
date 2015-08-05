af81bc73e9b1039d2ed16a3180e2a8faf9d7393b
Tamás Tóth <tamas.toth@mecharex.com>
2015-08-05 22:16:58 +0200

    Code School - Shaping up with Angular.JS - Level 1, Section 3, multiple gems

--- CodeSchool/GemStore/web/js/app.js
+++ CodeSchool/GemStore/web/js/app.js
@@ -1,14 +1,13 @@
-(function(){
-    var gem = {
-        name: 'Azurite',
-        price: 110.50,
-        canPurchase: false,
-        soldOut: true
-    };
-
+(function() {
     var app = angular.module('gemStore', []);
 
-    app.controller('StoreController', function() {
-        this.product = gem;
+    app.controller('StoreController', function(){
+        this.products = gems;
     });
+
+    var gems = [
+        { name: 'Azurite', price: 2.95 },
+        { name: 'Bloodstone', price: 5.95 },
+        { name: 'Zircon', price: 3.95 }
+    ];
 })();
