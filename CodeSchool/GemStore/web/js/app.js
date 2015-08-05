7012baedea89c73f4cbf75038355d0e473c44139
Tamás Tóth <tamas.toth@mecharex.com>
2015-08-05 23:13:28 +0200

    Code School - Shaping up with Angular.JS - Level 2 Section 2, gallery

--- CodeSchool/GemStore/web/js/app.js
+++ CodeSchool/GemStore/web/js/app.js
@@ -1,6 +1,14 @@
 (function() {
     var app = angular.module('gemStore', []);
 
+    app.controller('GalleryController', function() {
+        this.current = 0;
+
+        this.setCurrent = function(current) {
+            this.current = current || 0;
+        };
+    });
+
     app.controller('StoreController', function() {
         this.products = gems;
     });
