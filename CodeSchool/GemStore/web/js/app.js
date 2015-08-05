648af1be7065ea7a54c972afe070eb689d84592b
Tamás Tóth <tamas.toth@mecharex.com>
2015-08-05 22:13:55 +0200

    Code School - Shaping up with Angular.JS - Level 1, Section 3

--- CodeSchool/GemStore/web/js/app.js
+++ CodeSchool/GemStore/web/js/app.js
@@ -1,5 +1,11 @@
 (function(){
-    var gem = { name: 'Azurite', price: 2.95 };
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: true
+    };
+
     var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function() {
