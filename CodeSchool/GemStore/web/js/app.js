08527c7f0b22301a4e5144cacf07e4ea96670640
Tamás Tóth <tamas.toth@mecharex.com>
2015-08-06 19:03:55 +0200

    Code School - Shaping up with Angular.JS - Level 3 Section 3, createdOn

--- CodeSchool/GemStore/web/js/app.js
+++ CodeSchool/GemStore/web/js/app.js
@@ -29,6 +29,7 @@
         this.review = {};
 
         this.addReview = function(product) {
+            this.review.createdOn = Date.now();
             product.reviews.push(this.review);
             this.review = {};
         };
